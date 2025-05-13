ef5822e291782de9e9522b09a487f771d4b5a961
Rayen1a <raihan01426@gmail.com>
2025-05-13 10:34:37 +0700

    membuat class mahasiswa dan definisi constructor dengan parameter

---
+++ constructorberparameter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+    public :
+    mahasiswa(int nim, string nama); // Constuctor dengan parameter
+};
+//Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
