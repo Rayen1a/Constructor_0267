bb59676c291c27324328bb856b9b7224b6abadbf
Rayen1a <raihan01426@gmail.com>
2025-05-13 10:13:25 +0700

    first commit

---
+++ latihan.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//buatlah sebuah class bangun datar 
+class bangunDatar {
+    public :
+    int hitungLuas(int l, int p);
+    int hitungKeliling(int l, int p);
+};
+
+class persegiPanjang{
+    public :
+    int panjang, lebar;
+
+    friend int bangunDatar::hitungLuas(int l, int p);
+    friend int bangunDatar::hitungKeliling(int l, int p);
+};
+
+int bangunDatar::hitungLuas(int l, int p){
+    return l * p;
+}
+
+int bangunDatar::hitungKeliling(int l, int p){
+    return (2 * p) + (2 * l);
+}
+
+int main(){
+    bangunDatar bD;
+    persegiPanjang pP;
+    persegiPanjang &refpP = pP;
+    refpP.panjang = 10;
+    refpP.lebar = 100;
+    cout << bD.hitungLuas(refpP.lebar, refpP.panjang);
+}
